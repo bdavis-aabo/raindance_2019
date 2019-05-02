3655d4b283691da2e014a51efe25d465a9080767
Bryan <bdavis@artandbusinessone.com>
2019-05-02 13:27:16 -0600

    update to homepage lightbox

--- wp-content/themes/raindance-0.95/home/video.php
+++ wp-content/themes/raindance-0.95/home/video.php
@@ -2,7 +2,15 @@
   <div class="lightbox">
     <button class="close-btn"><i class="fa fa-times"></i></button>
 
+	<a href="http://bryans-macbook-pro.local:5757/grand-opening-festival">
+	      <img src="<?php bloginfo('template_directory') ?>/assets/images/raindance_event-popup.jpg" alt="Raindance Grand Opening Event" class="img-responsive aligncenter" />
+   	</a>
+    <?php /*
     <div class="video-responsive">
+	   
       <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0"width="640.8" height="360" type="text/html" src="https://www.youtube.com/embed/NPsd19JkOVQ?&theme=dark&autohide=2&modestbranding=1&fs=0&rel=0"></iframe>
+      
+      
     </div>
+    */ ?>
   </div>
